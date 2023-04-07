618808374b3f96ab0d769e0bb70fd336d0c99661
michalmazurdev <m.mazur40@gmail.com>
2023-04-07 14:31:51 +0200

    src files matched and loaded

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,19 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+let markup = ``;
+
+for (const image of galleryItems) {
+  markup += `<li> <a href=${image.original} class='gallery__item'> <img class='gallery__image' src=${image.preview} alt=${image.description}></a>
+  </li>`;
+}
+galleryEl.innerHTML = markup;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'outside',
+});
