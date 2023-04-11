0797710bcc418121462dcf77eb0e01da64e5fec7
michalmazurdev <m.mazur40@gmail.com>
2023-04-11 22:49:37 +0200

    2nd task started

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+const throttle = require('lodash.throttle');
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('play', function (data) {
+  console.log('play');
+  // console.log('played the video!');
+});
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
