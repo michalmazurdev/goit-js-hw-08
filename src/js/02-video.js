6940a21972370168644435086e78ab695b6b4779
michalmazurdev <m.mazur40@gmail.com>
2023-04-12 13:30:53 +0200

    throttle implemented to 2nd task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,19 @@
-import * as _ from 'lodash.throttle';
+import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
+
 player.on('timeupdate', function (data) {
-  _.throttle(
-    localStorage.setItem('videoplayer-current-time', data.seconds),
-    1000
-  );
   console.log(data);
 });
-player.on('timeupdate', function (data) {
-  console.log(data);
-});
+
 // player.on('timeupdate', function (data) {
 //   localStorage.setItem('videoplayer-current-time', data.seconds);
 // });
