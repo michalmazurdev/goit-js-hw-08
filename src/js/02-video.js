7d1fb19db9b2f50f1c0a1e6868e534f96c9981c2
michalmazurdev <m.mazur40@gmail.com>
2023-04-12 11:32:53 +0200

    trying to implement throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,18 @@
-const throttle = require('lodash.throttle');
+import * as _ from 'lodash.throttle';
 import Player from '@vimeo/player';
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
-function updateTime(data) {
-  localStorage.setItem('videoplayer-current-time', data.seconds);
-}
-player.on('timeupdate', _.throttle(updateTime, 1000));
-
+player.on('timeupdate', function (data) {
+  _.throttle(
+    localStorage.setItem('videoplayer-current-time', data.seconds),
+    1000
+  );
+  console.log(data);
+});
+player.on('timeupdate', function (data) {
+  console.log(data);
+});
 // player.on('timeupdate', function (data) {
 //   localStorage.setItem('videoplayer-current-time', data.seconds);
 // });
